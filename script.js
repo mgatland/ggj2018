c1b8db2e4a93224486d187bbf794298c459aecd0
Matthew <mgatland@gmail.com>
2018-01-27 17:23:11 +1300

    draw some wall textures

--- script.js
+++ script.js
@@ -44,7 +44,8 @@ function setDirs(dirs) {
 }
 
 const pos = {x: 16, y: 1, dir: dir.right}
-const depth = 0
+let depth = 0
+let tileSet = 0
 const map = []
 const enemies = []
 makeMap()
@@ -170,10 +171,7 @@ function draw3D(viewX, viewY, viewSize, dir) {
       if (cell == 0) {
         const left = viewXCentre - size/2 + j*size
         const top = viewYCentre - size/2
-        tCtx.fillStyle = "lightblue" //"darkgreen"
-        tCtx.fillRect(left, top, size, size)
-        tCtx.strokeStyle = "green"
-        tCtx.strokeRect(left, top, size, size)
+        tCtx.drawImage(spriteImage, 0, 256*tileSet, 256, 256, left, top, size, size)
       } else {
         const e = enemies.find(e => e.x == cellPos.x && e.y == cellPos.y)
         if (e != undefined) {
@@ -181,7 +179,7 @@ function draw3D(viewX, viewY, viewSize, dir) {
           const eSize = viewSizeX/(Math.pow(depthFactor,i-0.5))
           const left = viewXCentre - size/2 + j*size
           const top = viewYCentre - size/2
-          tCtx.drawImage(spriteImage, 0, 0, 256, 256, left+(size-eSize)/2, top+(size-eSize)/2, eSize, eSize)
+          tCtx.drawImage(spriteImage, 256, 0, 256, 256, left+(size-eSize)/2, top+(size-eSize)/2, eSize, eSize)
         }
       } 
     }
@@ -316,3 +314,44 @@ const states = { main:{}, map:{}}
 
 let state = states.main
 
+function keystoneAndDisplayImage(ctx, img, x, y, pixelWidth, 
+                 scalingFactor) {
+  var h = img.height,
+       w = img.width,
+        
+      // The number of slices to draw.
+      numSlices = Math.abs(pixelWidth),
+        
+      // The width of each source slice.
+      sliceWidth = w / numSlices,
+        
+      // Whether to draw the slices in reverse order or not.
+      polarity = (pixelWidth > 0) ? 1 : -1,
+
+      // How much should we scale the width of the slice 
+      // before drawing?
+      widthScale = Math.abs(pixelWidth) / w,
+        
+      // How much should we scale the height of the slice 
+      // before drawing? 
+      heightScale = (1 - scalingFactor) / numSlices;
+
+      for(var n = 0; n < numSlices; n++) {
+
+    // Source: where to take the slice from.
+    var sx = sliceWidth * n,
+        sy = 0,
+        sWidth = sliceWidth,
+        sHeight = h;
+    
+    // Destination: where to draw the slice to 
+    // (the transformation happens here).
+    var dx = x + (sliceWidth * n * widthScale * polarity),
+        dy = y + ((h * heightScale * n) / 2),
+        dWidth = sliceWidth * widthScale,
+        dHeight = h * (1 - (heightScale * n));
+
+    ctx.drawImage(img, sx, sy, sWidth, sHeight, 
+                dx, dy, dWidth, dHeight);
+  }
+}
