54880e8f90627cd4814d623637322d0d3738edc0
Quintavius <MSNoverflow@gmail.com>
2018-01-27 13:43:59 +1300

    fix directions

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ function rnd(n) {
 
 const mapSize = 100
 const cellSize = 7
-const dir = {up:{name:"up", y:-1}, left:{name:"left",x:1}, down:{name:"down",y:1}, right:{name:"right",x:-1}}
-setDirs([dir.up, dir.left, dir.down, dir.right])
+const dir = {up:{name:"up", y:-1}, right:{name:"right",x:1}, down:{name:"down",y:1}, left:{name:"left",x:-1}}
+setDirs([dir.up, dir.right, dir.down, dir.left])
 function setDirs(dirs) {
   for(let i = 0; i < dirs.length; i++) {
     dirs[i].cw = dirs[(i+1)%dirs.length]
@@ -39,7 +39,7 @@ function setDirs(dirs) {
   }
 }
 
-const pos = {x: 5, y: 5, dir: dir.up}
+const pos = {x: 13, y: 1, dir: dir.left}
 const depth = 0
 const map = []
 makeMap()
@@ -79,20 +79,55 @@ function addRoom(minSize, maxSize, hallway) {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  draw3D()
   drawMap()
   ctx.font="12px Verdana"
-  ctx.fillStyle="white"
-  ctx.fillText("position: " + pos.x + ":" + pos.y + ":" + pos.dir.name, 12, 12)
+  ctx.fillStyle="black"
+  ctx.fillText("position: " + pos.x + ":" + pos.y + ":" + pos.dir.name, 12, 748)
   
 }
 
+function draw3D() {
+  const viewX = 700
+  const viewY = 0
+  const viewSize = 300
+  ctx.fillStyle = "darkgrey"
+  ctx.fillRect(viewX, viewY, viewSize, viewSize/2)
+  ctx.fillStyle = "darkblue"
+  ctx.fillRect(viewX, viewY+viewSize/2, viewSize, viewSize/2)
+  ctx.fillStyle = "darkgreen"
+  for (let i = 15; i > 0; i--) { //depth
+    const size = viewSize/(Math.pow(3,i-1))
+    for (let j = -5; j < 5; j++) { //across
+      const cell = cellAt(viewCellPos(pos, i, j))
+      if (cell == 0) {
+        ctx.fillRect(viewX+viewSize/2-size/2+j*size, viewY+viewSize/2-size/2, size-1, size-1)
+      }
+    }
+  }
+}
+
+function viewCellPos(pos, i, j)
+{
+  if (pos.dir == dir.down) {
+    return {x:pos.x-j, y:pos.y + i}
+  }
+  if (pos.dir == dir.up) {
+    return {x:pos.x+j, y:pos.y - i}
+  }
+  if (pos.dir == dir.right) {
+    return {x:pos.x+i, y:pos.y + j}
+  }
+  if (pos.dir == dir.left) {
+    return {x:pos.x-i, y:pos.y - j}
+  }
+}
+
 function drawMap() {
-  const width = 100
-  const height = 100
   ctx.fillStyle = "grey"
-  ctx.fillRect(0, 0, width*cellSize, height*cellSize)
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
+  ctx.fillRect(0, 0, mapSize*cellSize, mapSize*cellSize)
+  for (let x = 0; x < mapSize; x++) {
+    for (let y = 0; y < mapSize; y++) {
       const cell = cellAt(x, y)
       if (cell == 1) {
         drawCell(x, y)
@@ -169,11 +204,19 @@ function turnRight() {
   draw()
 }
 function forward() {
-  const dest = {x:pos.x + pos.dir.x, y:pos.y + pos.dir.y}
+  const dest = move(pos, pos.dir)
   if (cellAt(dest.x, dest.y) == 1) {
     pos.x += pos.dir.x
     pos.y += pos.dir.y
   }
   draw()
 }
+function move(pos, move) {
+  return {x: pos.x + move.x, y: pos.y + move.y}
+}
 function times(n,f) {while(n-->0)f()}
+
+const states = { main:{}}
+
+let state = states.main
+
